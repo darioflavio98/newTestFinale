ae8c0ea177dec81a010ee67e15c8f7e5952fb00e
dario flavio <d.scipioni@dstech.it>
2025-02-25 13:45:35 +0100

    primaCommit

---
+++ infrastruttura/src/main/java/it/testFinale/infrastruttura/InfrastrutturaApplication.java
@@ -0,0 +1,13 @@
+package it.testFinale.infrastruttura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastrutturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrastrutturaApplication.class, args);
+	}
+
+}
---
+++ infrastruttura/src/test/java/it/testFinale/infrastruttura/InfrastrutturaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.testFinale.infrastruttura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastrutturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
